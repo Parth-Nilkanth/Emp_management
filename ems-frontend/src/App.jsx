50c7825b4dd733f91c000952ef6601cd8d196393
Parth-Nilkanth <138288152+Parth-Nilkanth@users.noreply.github.com>
2026-01-30 20:25:04 +0530

    added header and footer

--- ems-frontend/src/App.jsx
+++ ems-frontend/src/App.jsx
@@ -4,11 +4,15 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Hello from './Hello'
 import ListEmployeeComponent from './components/ListEmployeeComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
 
 function App() {
   return (
     <>
+      <HeaderComponent/>
       <ListEmployeeComponent/>
+      <FooterComponent/>
     </>
   )
 }
